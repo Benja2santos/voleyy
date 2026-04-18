3d08e4ddf598b6c09436698676a1f21cb5003d40
Benja2santos <benjamindossantos1003@gmail.com>
2026-04-18 14:52:31 -0300

    Comentarios, documentacion base

--- Marcador voley/Marcador/js/script.js
+++ Marcador voley/Marcador/js/script.js
@@ -1,11 +1,13 @@
+// VARIABLES GLOBALES
 let scoreA = 0;
 let scoreB = 0;
 
-let numero_sets = 1;
+let numero_sets = 1; //CONTADOR DE SETS (arranca de 1 porque es el primer ser)
 
-let campeonA = 0;
+let campeonA = 0;//EL QUE LLEGUE PRIMERO A 2 SETS ES CAMPEON
 let campeonB = 0;
 
+//AUMENTO DE PUNTOSSS
 function addPoint(team) {
     if ( team === 'A'){
         scoreA++;
@@ -16,7 +18,7 @@ function addPoint(team) {
     }
     setScore()
 }
-
+//BAJAR PUNTOS
 function subtrackPoint(team) {
     if ( team === 'A' && scoreA > 0){
         scoreA--;
@@ -26,8 +28,9 @@ function subtrackPoint(team) {
         document.getElementById('scoreB').textContent = scoreB;
     }
 }
+//EL QUE LLEGUE A 25PTS GANA EL SET
 function setScore(){
-    const diferencia = Math.abs(scoreA - scoreB);
+    const diferencia = Math.abs(scoreA - scoreB);//VARIABLE PARA COMPARAR VALORES
 
     if (( scoreA >= 25 || scoreB >= 25) && diferencia >= 2){
         if (scoreA > scoreB){
@@ -37,22 +40,24 @@ function setScore(){
             scoreB++;
             alert("El equipo ganador es: Equipo B")
         }
-        resetScore();
-        sets();
+        resetScore();//REINICIA EL CONTADOR
+        sets();//SUMA 1 SET
     }
 
 }
+//AUMENTA LOS SETS
 function sets(){
     numero_sets ++;
     document.getElementById('sets').textContent = numero_sets;
 }
+//RESETEADORRR
 function resetScore(){
     scoreA = 0;
     scoreB = 0;
     document.getElementById('scoreA').textContent = scoreA;
     document.getElementById('scoreB').textContent = scoreB;
 }
-
+//DEFINE EL GANADOR FINAL (para terminar)
 function campeon(){
     /* se juegan 2 sets y que gane 2 sets de seguido es campeon.
     pero si empatan 1==1 se juega un 3er set a 15 puntos.
